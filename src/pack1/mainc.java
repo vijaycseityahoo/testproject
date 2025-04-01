5b1b32690fb0143ae81d5edd4fab0754e7abfe08
vijay <vijay@LAPTOP-C01RVA2I>
2025-04-01 12:21:21 +0530

    commit2

--- src/pack1/mainc.java
+++ src/pack1/mainc.java
@@ -5,6 +5,7 @@ public class mainc {
 		public static void main(String[] args) {
 			System.out.println("first java program");
 			System.out.println("second java program");
-			
+			System.out.println("My third java program");
+			System.out.println("fourth project");
 		}	
 }
